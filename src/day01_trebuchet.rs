658402ebde7d352ad036d948d524a051af5a1d09
Rudy <rudy@hasbun.it>
2025-01-14 19:30:52 +0100

    day 1

---
+++ src/day01_trebuchet.rs
@@ -0,0 +1,94 @@
+﻿use crate::common::{Context, InputProvider};
+use std::collections::HashMap;
+
+pub fn run(context: &mut Context) {
+    context.add_test_inputs(get_test_inputs());
+
+    let input = context.get_input();
+    let input = input.as_str();
+
+    let mut sum = 0;
+    for word in input.lines() {
+        let first = if let Some(first) = word.chars().find(|c| c.is_numeric()) {
+            first
+        } else {
+            continue;
+        };
+        let last = word.chars().filter(|c| c.is_numeric()).last().unwrap();
+        let value = format!("{}{}", first, last).parse::<u32>().unwrap();
+        sum += value;
+    }
+
+    println!("part 1 sum: {}", sum);
+
+    let map = HashMap::from([
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ]);
+
+    let mut sum = 0;
+    for word in input.lines() {
+        let mut first = None;
+        let mut last = None;
+
+        for i in 0..word.len() {
+            let word = &word[i..];
+            let value = if let Some(value) = word
+                .chars()
+                .map(|c| if c.is_numeric() { Some(c) } else { None })
+                .next()
+                .flatten()
+            {
+                value.to_string()
+            } else if let Some(value) = map
+                .iter()
+                .filter(|(&text, _)| word.starts_with(text))
+                .map(|(_, value)| value)
+                .next()
+            {
+                value.to_string()
+            } else {
+                continue;
+            };
+
+            if first.is_none() {
+                first = Some(value.clone());
+            }
+            last = Some(value);
+        }
+
+        let value = format!("{}{}", first.unwrap(), last.unwrap())
+            .parse::<u32>()
+            .unwrap();
+        log::debug!("value: {}", value);
+        sum += value;
+    }
+
+    println!("part 2 sum: {}", sum);
+}
+
+fn get_test_inputs() -> impl Iterator<Item = Box<InputProvider>> {
+    [
+        "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet",
+        "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen",
+        "nineight",
+    ]
+    .into_iter()
+    .map(|input| Box::new(move || input.into()) as Box<InputProvider>)
+}
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use utils::timer::Timer;
+
+mod common;
+mod day01_trebuchet;
+
+fn main() {
+    let mut context = common::Context::default();
+
+    if let Ok(testing) = std::env::var("APP_TESTING") {
+        if let Ok(testing) = testing.parse() {
+            context.set_testing(testing);
+        }
+    }
+    //context.set_testing(0);
+
+    if std::env::var("RUST_LOG").is_err() {
+        if context.is_testing() {
+            std::env::set_var("RUST_LOG", "trace");
+        } else {
+            std::env::set_var("RUST_LOG", "info");
+        }
+    }
+    env_logger::init();
+    let _timer = Timer::start(|elapsed| println!("main took {} ms", elapsed.as_millis()));
+
+    let days = days();
+
+    let day: usize = if let Some(arg1) = std::env::args().nth(1) {
+        arg1.parse().expect("Failed to parse day number")
+    } else {
+        days.len()
+    };
+
+    context.set_text_input(Box::new(move || {
+        std::fs::read_to_string(format!("input/{:02}.txt", day))
+            .unwrap_or_else(|_| panic!("Failed to read input file input/{:02}.txt", day))
+            .into()
+    }));
+    let run = days[day - 1];
+
+    println!("Running day {}\n", day);
+    run(&mut context);
+}
+
+fn days() -> &'static [fn(&mut common::Context)] {
+    &[
+        day01_trebuchet::run,
+    ]
+}
